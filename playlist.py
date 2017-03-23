90d30fe67450a4c79309daad1b16e313804fdcfb
askjfhj <skdhfkj@slkhg.com>
2017-03-23 16:13:51 +0000

    first
    a file that does stuff..

---
+++ playlist.py
@@ -0,0 +1,122 @@
+#!/usr/local/bin/python3
+
+import os, shutil, argparse, subprocess
+
+files = []
+
+def process_cmdline():
+	parser = argparse.ArgumentParser(description='Swinsian Playlist - file copier')
+		
+	parser.add_argument('-p', '--playlist', help='playlist to load',required=True)
+	parser.add_argument('-o', '--destination', help='directory to copy files to',required=True)
+	
+	args = parser.parse_args()
+	
+	global playlist, destination
+	
+	playlist = args.playlist
+	destination = args.destination
+
+
+def os_command_output(command_run):
+	command_execute = subprocess.Popen(command_run, stdout=subprocess.PIPE, shell=True)
+	(command_execute_output, err) = command_execute.communicate()
+	command_execute_status = command_execute.wait()
+	#print "Command output : ", command_execute_output
+	#print "Command exit status/return code : ", command_execute_status
+
+	return command_execute_output
+
+def create_file_array():
+	if os.path.isfile(playlist):
+		f = open(playlist, encoding="utf-8").read().splitlines()
+
+		for line in iter(f):
+			if not line.startswith("#"):
+				#print line
+				fullfile = "\"/"+line.strip("../")+"\""
+							
+				try:
+					os.path.isfile(fullfile)
+				#print fullfile
+				
+
+					OS_AUDIO_INFO = "mediainfo --Language=raw \"--output=Audio;%Format%|%BitDepth/String%|%Format/Info%\" "+ fullfile
+					OS_AUDIO_INFO2 = "mediainfo --Language=raw \"--output=General;%Performer%|%Album%|%Track/Position%|%Track%\" "+ fullfile
+
+					#FILE_INFO=str(os_command_output(OS_AUDIO_INFO)).rstrip()
+					#FILE_INFO2=str(os_command_output(OS_AUDIO_INFO2))
+					#FILE_DETAILS=FILE_INFO.rstrip()+"|"+FILE_INFO2.rstrip()+"|"+fullfile
+					
+					FILE_INFO=os_command_output(OS_AUDIO_INFO).decode('utf-8')
+					FILE_INFO2=os_command_output(OS_AUDIO_INFO2).decode('utf-8')
+					FILE_DETAILS=FILE_INFO.rstrip()+"|"+FILE_INFO2.rstrip()+"|"+fullfile
+					
+
+					#print (FILE_DETAILS)
+
+					files.append(FILE_DETAILS)
+
+					
+				except IOError as e:    
+					print (e, ":audio file does not exist")
+				
+			
+	else:
+		print ("FATAL: "+playlist+" does not exist")
+
+def process_files():
+
+	for entry in files:
+		codec, rate, type_full, artist, album, track_num, track_name, filefull = entry.split('|')
+		filename = os.path.basename(filefull).strip("\"")
+
+		dest_dir = os.path.join(destination, artist, album)
+
+		
+		#print filename
+		#print file
+		file2=filefull.strip("\"")
+		#print (file2)
+
+		if codec == 'ALAC':
+			filefull = convert_alac_to_flac(filefull,filename)
+			#print (filefull)
+			delete_src_flac = "y"
+		
+		if os.path.exists(file2):
+			os.makedirs(dest_dir, exist_ok=True)
+			#shutil.copyfile( file, dest_dir )
+
+			OS_COPY = "cp -v "+filefull+" \""+dest_dir+"\""
+			#print (OS_COPY)
+			os.system(OS_COPY)
+
+			if delete_src_flac == 'y':
+				OS_REMOVE_FILE = "rm -f " + filefull
+				os.system(OS_REMOVE_FILE)
+
+
+			
+
+def convert_alac_to_flac(filefull,filename):
+	name, file_extension = os.path.splitext(filename)
+	filefull_org = filefull
+	filefull = "\"/tmp/"+ name + ".flac\""
+
+	OS_AUDIO_CONVERT = "ffmpeg -v error -i " + filefull_org + " -c: flac " + filefull + " -y"
+	os.system(OS_AUDIO_CONVERT)
+
+	return filefull
+
+
+
+process_cmdline()
+create_file_array()
+process_files()
+
+
+
+
+
+
